f52c6c0ec5f417699f05b32268083de3d81024a5
Shahar <Shaharhadad123@gmail.com>
2022-05-04 13:33:14 +0300

    add connector function

---
+++ src/DataAccess/DatabaseFuctions.java
@@ -0,0 +1,48 @@
+package DataAccess;
+import java.sql.*;
+
+
+public class DatabaseFuctions
+{
+    public Connection getConnector()
+    {
+        String url = "";
+        try
+        {
+            Connection conn = DriverManager.getConnection(url, "root", "root");
+            return conn;
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException();
+        }
+    }
+
+    public void save(String username)
+    {
+        Connection conn;
+        try
+        {
+            conn = getConnector();
+            Statement st = conn.createStatement();
+            st.executeUpdate("INSERT INTO Customers " +
+                    "VALUES (1001, 'Simpson', 'Mr.', 'Springfield', 2001)");
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.toString());
+        }
+
+    }
+
+    public void update()
+    {
+
+    }
+
+    public void delete()
+    {
+
+    }
+
+}
