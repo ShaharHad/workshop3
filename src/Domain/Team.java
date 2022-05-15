d240f01eb8f270ca5bbfe1a6d18beb760c476c17
mazal1 <maharim@post.bgu.ac.il>
2022-05-15 13:25:22 +0300

    update

--- src/Domain/Team.java
+++ src/Domain/Team.java
@@ -9,6 +9,7 @@ public class Team
     ArrayList<Manager> managers;
     Owner owner;
 
+
     public Team(ArrayList<Player> players, ArrayList<Coach> coaches, ArrayList<Manager> managers, Owner owner) {
         this.players = players;
         this.coaches = coaches;
